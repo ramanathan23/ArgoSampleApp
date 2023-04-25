4df80a79bf4a19eb86bdf2c93bdbc1ad78a76ff6
ramanathan23 <c.ramanathan23@gmail.com>
2023-04-25 19:14:14 +0530

    first commit

---
+++ Tests/IntegrationTests/test_todolistapi.py
@@ -0,0 +1,21 @@
+import json
+import requests
+
+
+baseUrl = "http://localhost:8000"
+
+def test_post_todo():
+    path = "/todo"
+    response = requests.post(url=baseUrl+path)
+    assert response.status_code == 200
+
+def test_get_todo():
+    path = "/todo"
+    response = requests.get(url=baseUrl+path)
+    responseJson = json.loads(response.text)
+    assert response.status_code == 200
+    
+def test_patch_todo():
+    path = "/todo"
+    response = requests.patch(url=baseUrl+path, json={"id": 0})
+    assert response.status_code == 200

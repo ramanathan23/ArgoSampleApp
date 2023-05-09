dada7b9795c24995be537650c386592fc9211fb7
ramanathan23 <c.ramanathan23@gmail.com>
2023-05-09 22:03:15 +0530

    docker file update commit

--- Tests/IntegrationTests/run.sh
+++ Tests/IntegrationTests/run.sh
@@ -3,5 +3,7 @@ echo $(ls /app)
 python -m pytest --html=report.html /app
 echo $(ls /)
 echo $(ls /app)
+cp assets /app/testresults
 cp *.html /app/testresults
 echo $(ls /app)
+echo $(ls /app/testresults)
